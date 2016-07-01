75a63d9e1070a744421dec974390afad9973ad06
Ryan Peach <rgpeach10@gmail.com>
2016-07-01 19:29:12 +0000

    Clean slate

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from distutils.core import setup
+from Cython.Build import cythonize
+import numpy as np
 
-setup(name='SurvivalAI',
-      version='0',
-      description='An AI for Minecraft and Minetest',
-      author='Ryan Peach',
-      author_email='ryan.peach@outlook.com',
-      url='http://www.github.com/ryanpeach/SurvivalAI',
-      packages=['numpy', 'scipy', 'pandas', 'https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.7.1-cp34-none-linux_x86_64.whl'],
-     )
+setup(
+    name = "SurvivalAI",
+    ext_modules = cythonize(["src/*.pyx"], include_path = ['./', np.get_include()]),
+)
